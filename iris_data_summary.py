e1e751b3557da3f0863cc0cfe774ffa7270a621a
Rishi Kumar <rk290695@gmail.com>
2024-04-15 15:48:24 +0530

    Update iris_data_summary.py

--- iris_data_summary.py
+++ iris_data_summary.py
@@ -8,9 +8,11 @@ from sklearn.decomposition import PCA
 
 
 f = Functions()
+
 iris = datasets.load_iris()
 iris = pd.DataFrame(iris.data)
 iris = iris.rename(columns = {'0':'SepalLengthCm','1':'SepalWidthCm','2':'PetalLengthCm','3':'PetalWidthCm'})
 
-print(iris)
-print(iris.describe())
+f.save_table(iris, name="IRIS DataSet")
+summary = iris.describe()
+f.save_table(summary, name="Summary Statistics")
