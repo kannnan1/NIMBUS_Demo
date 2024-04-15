eeee5f66b26ffe5b01d9dc34fcad9e370f7ac776
Rishi Kumar <rk290695@gmail.com>
2024-04-15 21:28:03 +0530

    Update iris_data_summary.py

--- iris_data_summary.py
+++ iris_data_summary.py
@@ -13,6 +13,6 @@ iris = datasets.load_iris()
 iris = pd.DataFrame(iris.data)
 iris = iris.rename(columns = {'0':'SepalLengthCm','1':'SepalWidthCm','2':'PetalLengthCm','3':'PetalWidthCm'})
 
-f.save_table(iris, name="IRIS DataSet")
-summary = iris.describe()
+summary = iris.describe().rename_axis('Metric').reset_index().round(2)
 f.save_table(summary, name="Summary Statistics")
+f.save_table(iris, name="IRIS DataSet")
